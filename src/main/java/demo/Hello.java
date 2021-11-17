3e8a3cc53b3394191cee53a64a9a07ae6a23d083
zhaojie <1979170837@qq,com>
2021-11-17 12:27:28 +0800

    initial.

---
+++ src/main/java/demo/Hello.java
@@ -0,0 +1,12 @@
+package demo;
+
+/**
+ * @author Jie Zhao
+ * @date 2021/11/17 10:42
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
